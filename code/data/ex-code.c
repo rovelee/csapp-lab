82c1c2af3375ae45a9376405cbd75eb4edc402b8
rovelee <rovely.lrv@gmail.com>
2022-02-03 00:06:58 +0800

    add ex-code and ex-test

---
+++ code/data/ex-code.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void ex213(){
+    int x = 0x66; // 0110 0110
+    int y = 0x93; // 1001 0011
+    printf("x & y = %x\n", x & y); // 0000 0010
+    printf("x | y = %x\n", x | y); // 1111 0111
+    printf("~x | ~y = %x\n", ~x | ~y ); // 1111 1101
+    printf("x & !y = %x\n", x & !y); // 0110 0101
+    printf("x && y = %x\n", x && y ); // 1
+    printf("x || y = %x\n", x || y ); // 1
+    printf("!x || !y = %x\n", !x || !y); // 0
+    printf("x && ~y = %x\n", x && ~y); // 1
+}
+
+int fn0_ex214(int x, int y)
+{
+    return x==y;
+}
+int fn1_ex214(int x, int y){
+    return (!x || y) && (!y || x);
+}
+void ex214(){
+    int x1 = 1, y1 = 1, x2 = 0, y2 = 0;
+    printf("f0 is x == y.\n");
+    printf("f1 is (!x || y) && (!y || x).\n");
+    printf("x\ty\tf0\tf1\n");
+    printf("%d\t%d\t%d\t%d\t\n", x1, y1, fn0_ex214(x1, y1), fn1_ex214(x1, y1));
+    printf("%d\t%d\t%d\t%d\t\n", x1, y2, fn0_ex214(x1, y2), fn1_ex214(x1, y2));
+    printf("%d\t%d\t%d\t%d\t\n", x2, y1, fn0_ex214(x2, y1), fn1_ex214(x2, y1));
+    printf("%d\t%d\t%d\t%d\t\n", x2, y2, fn0_ex214(x2, y2), fn1_ex214(x2, y2));
+}
---
+++ code/data/ex-test.c
@@ -0,0 +1,8 @@
+#include "ex-code.c"
+
+int main(int argc, char const *argv[])
+{
+    // ex213();
+    ex214();
+    return 0;
+}
